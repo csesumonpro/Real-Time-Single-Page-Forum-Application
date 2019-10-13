2fb88caf7894af9c021205d90aa2d269f85d208c
csesumonpro <csesumonpro@gmail.com>
2019-10-13 16:07:52 +0600

    category,question,like,replay model,controller,migration,factory added

---
+++ app/Http/Controllers/ReplayController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Replay;
+use Illuminate\Http\Request;
+
+class ReplayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Replay  $replay
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Replay $replay)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Replay  $replay
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Replay $replay)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Replay  $replay
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Replay $replay)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Replay  $replay
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Replay $replay)
+    {
+        //
+    }
+}
---
+++ database/factories/ReplayFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Replay;
+use Faker\Generator as Faker;
+
+$factory->define(Replay::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
