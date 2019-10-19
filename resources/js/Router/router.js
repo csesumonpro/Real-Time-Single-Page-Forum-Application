237ba4c32f13c1b8d0b7075c3c49ae1aef40d4bc
csesumonpro <csesumonpro@gmail.com>
2019-10-20 00:34:43 +0600

    Vue router added

---
+++ resources/js/Router/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    // { path: '/foo', component: Foo },
+]
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+
+export default router;
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,8 @@ import Vuetify from 'vuetify'
 Vue.use(Vuetify);
 Vue.component('AppHome',require('./components/AppHome.vue').default);
 
+import router from './Router/router.js'
 const app = new Vue({
     el: '#app',
+    router
 });
